89d20c0aa4f95505b86c1c408b83735463af45e0
Martin Rojas <commitearly@hologramfrog.xyz>
2025-10-19 14:07:54 -0600

    preview spanish translations

--- i18n/request.ts
+++ i18n/request.ts
@@ -2,7 +2,7 @@ import {getRequestConfig} from "next-intl/server"
 
 export default getRequestConfig(async () =>{
     // mock locale settings
-    const locale = 'en';
+    const locale = 'es';
 
     // fetch localized translation of the displayed content
     const messages= (await import(`../translations/${locale}.json`)).default
