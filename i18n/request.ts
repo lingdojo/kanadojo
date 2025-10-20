4918df4b65876d0ff882cd4cf76130ae01ebfd56
Martin Rojas <commitearly@hologramfrog.xyz>
2025-10-20 16:06:47 -0600

    set locale to English at helper

--- i18n/request.ts
+++ i18n/request.ts
@@ -2,7 +2,7 @@ import {getRequestConfig} from "next-intl/server"
 
 export default getRequestConfig(async () =>{
     // mock locale settings
-    const locale = 'es';
+    const locale = 'en';
 
     // fetch localized translation of the displayed content
     const messages= (await import(`../translations/${locale}.json`)).default
